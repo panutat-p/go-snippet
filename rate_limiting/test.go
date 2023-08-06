f82bfb1237a86c6a2969efa554725a34af9f92ca
Panutat <49073281+panutat-p@users.noreply.github.com>
2023-08-07 01:03:52 +0700

    feat: rate limiting

---
+++ rate_limiting/test.go
@@ -0,0 +1,36 @@
+package rate_limiting
+
+import (
+	"fmt"
+	"time"
+	
+	"golang.org/x/time/rate"
+)
+
+// https://medium.com/@isuruvihan/rate-limiting-in-go-controlling-traffic-with-efficiency-6a5ef7444ef8
+func TestRateLimit_fixed_window(t *testing.T) {
+	limiter := rate.NewLimiter(rate.Limit(100), 1) // Allow 100 requests per second
+	for i := 0; i < 200; i++ {
+		if !limiter.Allow() {
+			fmt.Println("Rate limit exceeded. Request rejected.")
+			continue
+		}
+		// Process the request
+		fmt.Println("Request processed successfully.")
+		time.Sleep(time.Millisecond * 100) // Simulate request processing time
+	}
+}
+
+// https://medium.com/@isuruvihan/rate-limiting-in-go-controlling-traffic-with-efficiency-6a5ef7444ef8
+func TestRateLimit_tojen_bucket(t *testing.T) {
+	limiter := rate.NewLimiter(rate.Limit(10), 5) // Allow 10 requests per second with a burst of 5
+	for i := 0; i < 200; i++ {
+		if !limiter.Allow() {
+			fmt.Println("Rate limit exceeded. Request rejected.")
+			continue
+		}
+		// Process the request
+		fmt.Println("Request processed successfully.")
+		time.Sleep(time.Millisecond * 100) // Simulate request processing time
+	}
+}
