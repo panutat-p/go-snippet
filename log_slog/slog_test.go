97cc94c480f33b3b035c1219dced7e0b9f5bcc05
Panutat <49073281+panutat-p@users.noreply.github.com>
2023-08-21 01:35:21 +0700

    test: slog text handler.go

---
+++ log_slog/slog_test.go
@@ -0,0 +1,24 @@
+package log_slog
+
+import (
+   "log/slog"
+)
+
+// https://pcpratheesh.medium.com/an-overview-of-slog-a-structured-logging-package-for-go-6dec67215b9a
+func TestSlog_text_handler(t *testing.T) {
+	h := slog.NewTextHandler(os.Stdout, nil)
+	logger := slog.New(h)
+	logger.Info(
+		"Demo",
+		slog.String("app-version", "v0.0.1"),
+		slog.Int("release-version", 1),
+		slog.Float64("point-value", 1.2),
+		slog.Bool("status", true),
+		slog.Time("time", time.Now()),
+		slog.Group(
+			"request",
+			slog.String("path", "https://example.com"),
+			slog.String("method", "GET"),
+		),
+	)
+}
