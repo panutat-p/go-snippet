e73f0f28d8c5fd4d55b8385bac6cd85cb3bba25d
Panutat <49073281+panutat-p@users.noreply.github.com>
2023-09-02 19:33:56 +0700

    feat: slog, json handler

--- log_slog/slog_test.go
+++ log_slog/slog_test.go
@@ -22,3 +22,21 @@ func TestSlog_text_handler(t *testing.T) {
 		),
 	)
 }
+
+func TestSlog_json_handler(t *testing.T) {
+	h := slog.NewJSONHandler(os.Stdout, nil)
+	logger := slog.New(h)
+	logger.Info(
+		"Demo",
+		slog.String("app-version", "v0.0.1"),
+		slog.Int("release-version", 1),
+		slog.Float64("point-value", 1.2),
+		slog.Bool("status", true),
+		slog.Time("time", time.Now()),
+		slog.Group(
+			"request",
+			slog.String("path", "https://example.com"),
+			slog.String("method", "GET"),
+		),
+	)
+}
