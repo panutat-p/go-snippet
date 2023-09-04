28876924608e337d5612d16db86de5c31aae740f
Panutat <49073281+panutat-p@users.noreply.github.com>
2023-09-05 02:44:35 +0700

    feat: custom validation

--- go_validator/domain.go
+++ go_validator/domain.go
@@ -4,3 +4,19 @@ type Profile struct {
 	Username string `validate:"required,alphanum,min=4,max=20"`
 	Password string `validate:"required,min=6,max=20"`
 }
+
+type Person struct {
+	Name   string `validate:"required"`
+	Height string `validate:"required,validHeight"`
+}
+
+func ValidateHeight(fl validator.FieldLevel) bool {
+	height, err := strconv.Atoi(fl.Field().String())
+	if err != nil {
+		return false
+	}
+
+	minHeight := 30
+	maxHeight := 300
+	return height >= minHeight && height <= maxHeight
+}
