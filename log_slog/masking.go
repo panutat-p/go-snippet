9c60d6c6cbb74ae09c88f3e7fc3a8e57e0772e85
Panutat <49073281+panutat-p@users.noreply.github.com>
2023-09-14 00:59:54 +0700

    fix: slog, LogValue

--- log_slog/masking.go
+++ log_slog/masking.go
@@ -5,15 +5,14 @@ import (
 )
 
 type Person struct {
-    Name  string
-    Email string
+	Name  string
+	Email string
 }
 
-func (p Person) LogValue() slog.LogValue {
+func (p Person) LogValue() slog.Value {
 	parts := strings.Split(p.Email, "@")
 	if len(parts) != 2 {
-		return slog.Struct(
-			"Person",
+		return slog.GroupValue(
 			slog.String("Name", p.Name),
 			slog.String("Email", p.Email),
 		)
@@ -22,8 +21,7 @@ func (p Person) LogValue() slog.LogValue {
 	localPart := parts[0]
 	domain := parts[1]
 	maskedLocalPart := strings.Repeat("*", len(localPart))
-
-	return slog.Struct("Person",
+	return slog.GroupValue(
 		slog.String("Name", p.Name),
 		slog.String("Email", fmt.Sprintf("%s@%s", maskedLocalPart, domain)),
 	)
