02ec39adf53042e53cbcef86f68e78c4dca85168
Panutat <49073281+panutat-p@users.noreply.github.com>
2023-09-17 01:05:53 +0700

    feat: errors wrap

---
+++ error_wrapping/error_test.go
@@ -0,0 +1,22 @@
+package error_wrapping
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+)
+
+// https://pkg.go.dev/errors#Join
+
+func TestErrors_join(t *testing.T) {
+	err1 := errors.New("err1")
+	err2 := errors.New("err2")
+	err := errors.Join(err1, err2)
+	fmt.Println(err)
+	if errors.Is(err, err1) {
+		fmt.Println("err is err1")
+	}
+	if errors.Is(err, err2) {
+		fmt.Println("err is err2")
+	}
+}
